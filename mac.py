e29d5214b06f568ce15cf2324a662a7eb42fb5f0
Minh Phan <anhminh1993@icloud.com>
2021-11-22 10:51:12 -0800

    reconstruct repo

--- mac.py
+++ mac.py
@@ -3,32 +3,34 @@
 import os
 import datetime
 import csv
+from typing import List
+
 from mdutils.mdutils import MdUtils
 #csv and markdown files
-def main():
+def mac():
     try:
-        with open('C:\\Users\\User\\Downloads\\Replay 2021.txt', 'r', encoding='utf-16') as file:
+        with open(os.path.expanduser('~/Downloads/replay 2021.txt'), 'r', encoding='utf-16') as file:
             raw = file.read().split('\n')[:101]
 
         #extract the necessary data
-        raw[0] = raw[0].split('\t')[:2] + raw[0].split('\t')[3:4] + raw[0].split('\t')[9:10] + raw[0].split('\t')[11:12] + raw[0].split('\t')[25:26]
-        raw[0].insert(0, 'Position')
-        for i in range(1, len(raw)):
-            raw[i] = raw[i].split('\t')[:2]+raw[i].split('\t')[3:4] + raw[i].split('\t')[9:10] + time(raw[i].split('\t')[11:12]) + raw[i].split('\t')[25:26]
-            raw[i].insert(0, i)
+            raw[0] = raw[0].split('\t')[:2] + raw[0].split('\t')[3:4] + raw[0].split('\t')[9:10] + raw[0].split('\t')[11:12] + raw[0].split('\t')[25:26]
+            raw[0].insert(0, 'Position')
+            for i in range(1, len(raw)):
+                raw[i] = raw[i].split('\t')[:2]+raw[i].split('\t')[3:4] + raw[i].split('\t')[9:10] + time(raw[i].split('\t')[11:12]) + raw[i].split('\t')[25:26]
+                raw[i].insert(0, i)
 
         # for i in raw:
         #     print(i[0], i[1], i[2], i[3], i[4], i[5], i[6])
-        os.remove('C:\\Users\\User\\Downloads\\Replay 2021.txt')
+        os.remove(os.path.expanduser('~/Downloads/replay 2021.txt'))
         [path, date] = check()
         os.chdir(path)
-        #print(*raw, sep='\n')
+        print(*raw, sep='\n')
         csvfile(raw, date)
         markdown(raw, date)
     except Exception as e:
         print(e)
 
-def csvfile(arr: list[str], date):
+def csvfile(arr: List[str], date):
     if not os.path.isfile(f"{date}.csv"):
         with open(f"{date}.csv","w",newline='', encoding='utf-8') as file:
             obj = csv.writer(file)
@@ -44,19 +46,20 @@ def markdown(arr: list, date):
     mdFile.new_table(columns=7, rows=101, text=temp, text_align='center')
     mdFile.create_md_file()
 
-def check()->list[str, str]:
-    dir = str(input("what is the date of that Sunday?"))
-    parent_dir = r"C:/Users/User/PycharmProjects/AppleMusicReplay/replay/replay21"
-    path = os.path.join(parent_dir,dir)
+
+def check() -> List[str]:
+    d = str(input("what is the date of that Sunday?"))
+    parent_dir = os.path.expanduser('~/Documents/apple-music-replay/replay/replay21')
+    path = os.path.join(parent_dir, d)
     try:
         os.mkdir(path)
     except FileExistsError:
         print("Already has directory")
     finally:
-        return [path, dir]
+        return [path, d]
 
-def time(l:list)-> list:
+def time(l:List)-> List:
     return [str(datetime.timedelta(seconds=int(l[0])))]
 
 if __name__ == "__main__":
-    main()
+    mac()
